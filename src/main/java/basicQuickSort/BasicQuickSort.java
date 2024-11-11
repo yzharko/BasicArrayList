e600ce66137ec22a430a45442c0173c4a6a68c67
Zharko <zharko.aktobe@gmail.com>
2024-11-11 14:50:25 +0300

    code: BasicArrayList compatibility has been added

--- src/main/java/basicQuickSort/BasicQuickSort.java
+++ src/main/java/basicQuickSort/BasicQuickSort.java
@@ -6,16 +6,16 @@ import java.util.*;
 
 public class BasicQuickSort {
     public static <T extends Comparable <T>>
-    void quicksort(T[] array) {
-        quicksort(array, 0, array.length - 1);
+    void quicksort(BasicArrayList <T> array) {
+        quicksort(array, 0, array.getLength() - 1);
     }
     private static <T extends Comparable <T>>
-    void quicksort(T[] array, int lowIndex, int highIndex) {
+    void quicksort(BasicArrayList <T> array, int lowIndex, int highIndex) {
         if (lowIndex >= highIndex) {
             return;
         }
         int pivotIndex = new Random().nextInt(highIndex - lowIndex) + lowIndex;
-        T pivot = array[pivotIndex];
+        T pivot = array.get(pivotIndex);
         swap(array, pivotIndex, highIndex);
 
         int leftPointer = partition(array, lowIndex, highIndex, pivot);
@@ -24,11 +24,11 @@ public class BasicQuickSort {
         quicksort(array, leftPointer + 1, highIndex);
     }
     private static <T extends Comparable <T>>
-    int partition(T[] array, int lowIndex, int highIndex, T pivot) {
+    int partition(BasicArrayList <T> array, int lowIndex, int highIndex, T pivot) {
         int leftPointer = lowIndex;
 
         for (int elem = lowIndex; elem < highIndex; ++elem) {
-            if (array[elem].compareTo(pivot) <= 0) {
+            if (array.get(elem).compareTo(pivot) <= 0) {
                 swap(array, elem, leftPointer);
                 leftPointer++;
             }
@@ -37,9 +37,9 @@ public class BasicQuickSort {
         return leftPointer;
     }
     private static <T>
-    void swap(T[] array, int firstIndex, int secondIndex) {
-        T temp = array[firstIndex];
-        array[firstIndex] = array[secondIndex];
-        array[secondIndex] = temp;
+    void swap(BasicArrayList <T> array, int firstIndex, int secondIndex) {
+        T temp = array.get(firstIndex);
+        array.set(firstIndex, array.get(secondIndex));
+        array.set(secondIndex, temp);
     }
 }
