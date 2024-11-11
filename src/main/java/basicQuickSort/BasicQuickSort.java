886463ae1f8f3d6837d70e880f226a1f3fd97fa3
Zharko <zharko.aktobe@gmail.com>
2024-11-11 16:26:09 +0300

    code: javadoc description for BasicQuickSort class and it's methods has been added

--- src/main/java/basicQuickSort/BasicQuickSort.java
+++ src/main/java/basicQuickSort/BasicQuickSort.java
@@ -4,11 +4,30 @@ import basicArrayList.BasicArrayList;
 
 import java.util.*;
 
+/**
+ * BasicQuickSort принимает объект BasicArrayList и сортирует его в восходящем порядке. Все элементы в коллекции должны реализовывать интерфейс Comparable.
+ * Кроме того, каждый из элементов коллекции болжны иметь возможность сравниваться друг с другом.
+ */
 public class BasicQuickSort {
+    /**
+     * Перегруженный метод quicksort для удобства пользования
+     * @param array BasicArrayList, который необходимо отсортировать
+     * @param <T> обязан реализовывать интерфейс Comparable
+     */
     public static <T extends Comparable <T>>
     void quicksort(BasicArrayList <T> array) {
         quicksort(array, 0, array.getLength() - 1);
     }
+    /**
+     * Осоновной рекурсивный метод quicksort. При большом количестве рекурсивных вызовов, может вызвать переполнения стека (!).
+     * В качестве pivot выбирается случайный элемент коллекции, находящийся между первым и последним элементами.
+     * После чего, выбранный элемент меняется местами с последним элементом. Далее, осуществляется разделение на разделы: "меньше текущего элемента"
+     * и "больше текущего эелемента", для которых рекурсивно вызывается метод quicksort
+     * @param array BasicArrayList, который необходимо отсортировать
+     * @param lowIndex первый элемент
+     * @param highIndex последний элемент
+     * @param <T> обязан реализовывать интерфейс Comparable
+     */
     private static <T extends Comparable <T>>
     void quicksort(BasicArrayList <T> array, int lowIndex, int highIndex) {
         if (lowIndex >= highIndex) {
@@ -23,6 +42,16 @@ public class BasicQuickSort {
         quicksort(array, lowIndex, leftPointer - 1);
         quicksort(array, leftPointer + 1, highIndex);
     }
+    /**
+     * Разделяет переданный BasicArrayList на разделы "меньше текущего элемента" и "больше текущего эелемента":
+     * если значение leftPointer равен или меньше чем pivot, то перемещаем налево от него, если больше, то перемещаем направо.
+     * @param array BasicArrayList, который необходимо отсортировать
+     * @param lowIndex первый элемент
+     * @param highIndex последний элемент
+     * @param pivot элемент, с которым происходит сравнение всех остальных элементов коллекции
+     * @return элемент на котором закончили сортировать переданную коллекцию
+     * @param <T>
+     */
     private static <T extends Comparable <T>>
     int partition(BasicArrayList <T> array, int lowIndex, int highIndex, T pivot) {
         int leftPointer = lowIndex;
@@ -36,6 +65,13 @@ public class BasicQuickSort {
         swap(array, highIndex, leftPointer);
         return leftPointer;
     }
+    /**
+     * Меняет местами элементы BasicArrayList по предоставленным индексам
+     * @param array BasicArrayList в котором осуществляется перестановка элементов
+     * @param firstIndex место первого элемента
+     * @param secondIndex место второго элемента
+     * @param <T>
+     */
     private static <T>
     void swap(BasicArrayList <T> array, int firstIndex, int secondIndex) {
         T temp = array.get(firstIndex);
